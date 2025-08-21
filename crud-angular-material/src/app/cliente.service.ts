29511faa55891bad71528eb51ff96c2bcbf85503
Iuri Souza <iuri.souzasantos@gmail.com>
2025-08-20 22:01:12 -0300

    Aula 36

--- crud-angular-material/src/app/cliente.service.ts
+++ crud-angular-material/src/app/cliente.service.ts
@@ -2,30 +2,38 @@ import { Injectable } from '@angular/core';
 import { Cliente } from './cadastro/cliente';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ClienteService {
+  static REPO_CLIENTES = '_CLIENTES';
 
-  static REPO_CLIENTES = "_CLIENTES"
+  constructor() {}
 
-  constructor() { }
-
-  salvar(cliente: Cliente){
+  salvar(cliente: Cliente) {
     const storage = this.obterStorage();
     storage.push(cliente);
 
     localStorage.setItem(ClienteService.REPO_CLIENTES, JSON.stringify(storage));
   }
 
-  obterStorage() : Cliente[]{
-    const repositorioClientes = localStorage.getItem(ClienteService.REPO_CLIENTES);
-    if(repositorioClientes){
-      const clientes : Cliente[] = JSON.parse(repositorioClientes);
+  pesquisarClientes(nome: string): Cliente[] {
+    return this.obterStorage();
+  }
+
+  private obterStorage(): Cliente[] {
+    const repositorioClientes = localStorage.getItem(
+      ClienteService.REPO_CLIENTES
+    );
+    if (repositorioClientes) {
+      const clientes: Cliente[] = JSON.parse(repositorioClientes);
       return clientes;
     }
 
     const clientes: Cliente[] = [];
-    localStorage.setItem(ClienteService.REPO_CLIENTES, JSON. stringify(clientes));
+    localStorage.setItem(
+      ClienteService.REPO_CLIENTES,
+      JSON.stringify(clientes)
+    );
     return clientes;
   }
 }
--- crud-angular-material/src/app/consulta/consulta.component.ts
+++ crud-angular-material/src/app/consulta/consulta.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { ClienteService } from '../cliente.service';
+import { Cliente } from '../cadastro/cliente';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatInputModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule,
+  ],
   templateUrl: './consulta.component.html',
-  styleUrl: './consulta.component.scss'
+  styleUrl: './consulta.component.scss',
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit {
+  listaClientes: Cliente[] = [];
 
+  constructor(private service: ClienteService) {}
+
+  ngOnInit() {
+    this.listaClientes = this.service.pesquisarClientes('');
+  }
 }
